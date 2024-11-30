84f14dbec5433c400d163766350b22d5b5fe1eea
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-11-30 17:42:10 +0900

    chore: init project

---
+++ src/main/java/stt/investmentsimulatorserver/InvestmentSimulatorServerApplication.java
@@ -0,0 +1,13 @@
+package stt.investmentsimulatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentSimulatorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentSimulatorServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stt/investmentsimulatorserver/InvestmentSimulatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package stt.investmentsimulatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentSimulatorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
